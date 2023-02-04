2b1b6fe520687f4c054f9164c53f0738d085553f
Charlie Holtz <charlieholtz@gmail.com>
2023-02-04 17:36:53 -0500

    sound effect oops

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
 import FileSaver from "file-saver";
+import useSound from "use-sound";
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
@@ -37,15 +38,16 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    // var cols = Math.min(Math.round(window.innerWidth / 256), 12);
-    // var rows = Math.min(Math.round(window.innerHeight / 256), 12) + 1;
-    var cols = 4;
-    var rows = 3;
+    var cols = Math.min(Math.ceil(window.innerWidth / 512), 12);
+    var rows = Math.min(Math.ceil(window.innerHeight / 512), 12);
     setTotal(cols * rows);
     setCols(`grid-cols-${cols}`);
     setRows(`grid-rows-${rows}`);
     console.log(cols, rows, total);
   }, []);
+
+  const [playActive] = useSound("/beep.mp3", { volume: 0.25 });
+  const [playSuccess] = useSound("/ding.wav", { volume: 0.25 });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -97,12 +99,18 @@ export default function Home() {
       tiles[i].classList.remove("animate-drop");
     }
 
+    const interval = setInterval(playActive, 100);
+
     setTimeout(() => {
       for (let i = 0; i < tiles.length; i++) {
         tiles[i].classList.add("animate-drop");
       }
       setWallpaper(image);
     }, 10);
+
+    setTimeout(() => {
+      clearInterval(interval), playSuccess();
+    }, 1700);
   };
 
   const stitchImages = async (imageUrl) => {
@@ -112,17 +120,28 @@ export default function Home() {
 
     var myCanvas = document.getElementById("canvas");
     var ctx = myCanvas.getContext("2d");
-    ctx.canvas.width = 2000;
-    ctx.canvas.height = 1000;
+
+    const screenHeight = window.screen.height * window.devicePixelRatio;
+    const screenWidth = window.screen.width * window.devicePixelRatio;
+
+    ctx.canvas.width = screenWidth;
+    ctx.canvas.height = screenHeight;
 
     var img = new Image();
     img.src = imageURL;
 
+    var x = 0;
+    var y = 0;
+
     img.addEventListener("load", (e) => {
-      ctx.drawImage(img, 0, 0); // Or at whatever offset you like
-      ctx.drawImage(img, 0, 512); // Or at whatever offset you like
-      ctx.drawImage(img, 512, 0); // Or at whatever offset you like
-      ctx.drawImage(img, 512, 512); // Or at whatever offset you like
+      while (y < screenHeight) {
+        while (x < screenWidth) {
+          ctx.drawImage(img, x, y);
+          x += 512;
+        }
+        x = 0;
+        y += 512;
+      }
     });
 
     ctx.fillStyle = "green";
@@ -144,117 +163,123 @@ export default function Home() {
   };
 
   return (
-    <div
-      className="relative min-h-screen"
-      //   style={{
-      //     backgroundImage: `url(${example.image})`,
-      //     backgroundRepeat: "repeat",
-      //     backgroundSize: "256px",
-      //   }}
-    >
-      <Head>
-        <title>Wallpaper Creator</title>
-      </Head>
+    <>
+      <div className="relative min-h-screen">
+        <Head>
+          <title>Wallpaper Creator</title>
+        </Head>
 
-      <div className={`grid ${rows} ${cols}`}>
-        {Array(total)
-          .fill(1)
-          .map((_value, index) => (
-            <img
-              key={`tile-${index}`}
-              id={index}
-              className="tile animate-drop"
-              style={{ animationDelay: `${index * 0.1}s` }}
-              src={wallpaper}
-              alt=""
-            />
-          ))}
-      </div>
+        <div className={`grid grid-cols-4 grid-rows-4`}>
+          {Array(total)
+            .fill(1)
+            .map((_value, index) => (
+              <img
+                key={`tile-${index}`}
+                id={index}
+                className="tile animate-drop"
+                style={{ animationDelay: `${index * 0.1}s` }}
+                src={wallpaper}
+                alt=""
+              />
+            ))}
+        </div>
 
-      <div className="fixed top-0 left-0">
-        <canvas id="canvas" className="fixed top-0 left-0"></canvas>
-      </div>
+        <div className="fixed hidden top-0 left-0">
+          <canvas id="canvas" className="fixed top-0 left-0"></canvas>
+        </div>
 
-      <form className="max-w-sm mx-auto absolute top-4" onSubmit={handleSubmit}>
-        <div>
-          <label
-            htmlFor="name"
-            className="block text-sm font-medium text-gray-700"
-          ></label>
-          <div className="mt-1 border-gray-300 focus-within:border-indigo-600">
-            <input
-              type="text"
-              name="prompt"
-              id="name"
-              className="block w-full rounded-lg bg-gray-50 focus:border-indigo-600 focus:ring-0 sm:text-sm"
-              placeholder="Enter your wallpaper prompt"
-            />
+        <form
+          className="max-w-sm mx-auto absolute top-4"
+          onSubmit={handleSubmit}
+        >
+          <div>
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            ></label>
+            <div className="mt-1 border-gray-300 focus-within:border-indigo-600">
+              <input
+                type="text"
+                name="prompt"
+                id="name"
+                className="block w-full rounded-lg bg-gray-50 focus:border-indigo-600 focus:ring-0 sm:text-sm"
+                placeholder="Enter your wallpaper prompt"
+              />
+            </div>
           </div>
-        </div>
 
-        <button
-          type="button"
-          onClick={() => {
-            resetWallpaper(
-              examples[Math.floor(Math.random() * examples.length)].image
-            );
-          }}
-        >
-          Set Wallpaper
-        </button>
+          <button
+            type="button"
+            onClick={() => {
+              resetWallpaper(
+                examples[Math.floor(Math.random() * examples.length)].image
+              );
+            }}
+          >
+            Set Wallpaper
+          </button>
 
-        <button
-          type="button"
-          className="bg-red-500 p-4"
-          onClick={() => {
-            download(wallpaper);
-          }}
-        >
-          Download
-        </button>
+          <button
+            type="button"
+            onClick={() => playActive()}
+            className="bg-orange-500 -4"
+          >
+            sound
+          </button>
 
-        <div className="mt-4 text-center">
-          {loading ? (
-            <button
-              type="submit"
-              disabled
-              className="inline-flex items-center rounded-md border border-transparent bg-indigo-800 px-6 py-3 text-base font-medium text-indigo-300 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            >
-              <div role="status">
-                <svg
-                  aria-hidden="true"
-                  class="w-6 h-6 mr-3 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                  viewBox="0 0 100 101"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                    fill="currentColor"
-                  />
-                  <path
-                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                    fill="currentFill"
-                  />
-                </svg>
-                <span class="sr-only">Loading...</span>
-              </div>
-              Creating your wallpaper...
-            </button>
-          ) : (
-            <button
-              type="submit"
-              className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            >
-              Create your wallpaper
-            </button>
-          )}
-        </div>
-      </form>
+          <button
+            type="button"
+            className="bg-red-500 p-4"
+            onClick={() => {
+              download(wallpaper);
+            }}
+          >
+            Download
+          </button>
 
-      {error && <div>{error}</div>}
+          <div className="mt-4 text-center">
+            {loading ? (
+              <button
+                type="submit"
+                disabled
+                className="inline-flex items-center rounded-md border border-transparent bg-indigo-800 px-6 py-3 text-base font-medium text-indigo-300 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <div role="status">
+                  <svg
+                    aria-hidden="true"
+                    class="w-6 h-6 mr-3 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span class="sr-only">Loading...</span>
+                </div>
+                Creating your wallpaper...
+              </button>
+            ) : (
+              <button
+                type="submit"
+                className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Create your wallpaper
+              </button>
+            )}
+          </div>
+        </form>
 
-      {prediction && <p>status: {prediction.status}</p>}
-    </div>
+        {error && <div>{error}</div>}
+
+        {prediction && <p>status: {prediction.status}</p>}
+      </div>
+    </>
   );
 }
