87597467effefa0f7dee08c475c90bdcc4d83a79
Charlie Holtz <charlieholtz@gmail.com>
2023-02-07 17:39:57 -0500

    frame

--- pages/index.js
+++ pages/index.js
@@ -320,6 +320,11 @@ export default function Home() {
         <Head>
           <title>Tiled Stable Diffusion</title>
 
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🖼️</text></svg>"
+          />
+
           <meta
             property="og:description"
             content="Make your next wallpaper with tiled stable diffusion"
