fcec12c19b649c126993f37db7f9f779199f5670
Charlie Holtz <charlieholtz@gmail.com>
2023-02-09 13:15:48 -0500

    summary code

--- pages/index.js
+++ pages/index.js
@@ -355,6 +355,7 @@ export default function Home() {
           <meta property="og:image" content="/tile.png" />
           <meta property="twitter:image" content="/tile.png" />
           <meta property="twitter:title" content={appName} />
+          <meta name="twitter:card" content="summary_large_image" />
 
           <meta
             name="viewport"
